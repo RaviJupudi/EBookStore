f080cef0204083056628b58237f5fac3e27f8e36
RaviJupudi <ravi.jupudii@gmail.com>
2025-04-23 12:33:43 -0700

    build issue

--- src/main/java/com/jupudi/books/ebookstore/EBookStoreApplication.java
+++ src/main/java/com/jupudi/books/ebookstore/EBookStoreApplication.java
@@ -3,7 +3,10 @@ package com.jupudi.books.ebookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+})
 public class EBookStoreApplication {
 
 	public static void main(String[] args) {
